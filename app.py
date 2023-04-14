556abd0a489a43d829548c126f6b4bc2e13afccb
Abdul <abdulbasirqazi7896@gmail.com>
2023-04-15 01:52:36 +0500

    now the data is sended in database very well. like image data

--- app.py
+++ app.py
@@ -1,74 +1,124 @@
-from flask import Flask ,request, render_template ,  flash , redirect
-from database import log_in_from_db , add_sign_table_to_db
-from database import engine , text 
+from flask import Flask, request, render_template,  flash, redirect
+from database import log_in_from_db, add_sign_table_to_db
+from database import engine, text
 import os
+from datetime import datetime
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home_page():
     return render_template('index.html')
+
 
 @app.route("/sign_in")
 def sign_page():
     return render_template("sign_page.html")
 
 
-
-@app.route("/sign"  , methods =["GET" ,"POST"])
+@app.route("/sign", methods=["GET", "POST"])
 def sign_page_for():
     print("this is sign_page")
     if request.method == "POST":
         with engine.connect() as conn:
-            query = text("INSERT INTO sign_table (email__add, password_has, address1, address2, city) VALUES (:email__add, :password_has, :address1, :address2, :city)")
+            query = text(
+                "INSERT INTO sign_table (email__add, password_has, address1, address2, city) VALUES (:email__add, :password_has, :address1, :address2, :city)")
 
-            conn.execute(query, {"email__add": request.form.get('email'), "password_has": request.form.get("password"), "address1": request.form.get("address1"), "address2": request.form.get("address2"), "city": request.form.get("city")})
+            conn.execute(query, {"email__add": request.form.get('email'), "password_has": request.form.get(
+                "password"), "address1": request.form.get("address1"), "address2": request.form.get("address2"), "city": request.form.get("city")})
+            print("value is inserted in database")
             return render_template("log_in.html")
     return render_template("sign_page.html")
-        
 
 
-@app.route("/log_in" , methods =["GET", "POST"])
+@app.route("/log_in", methods=["GET", "POST"])
 def log_page():
-    print("thisis log_page now")    
+    print("thisis log_page now")
     if request.method == "POST":
         email = request.form.get("email")
         password = int(request.form.get("password"))
         log = log_in_from_db()
+        print("THis is code ")
+        print(log)
         for code in log:
             if email == "abdulbasirqazi@gmail.com" and password == 12345:
                 return render_template("admin.html")
+
             if email == code[0] and password == code[1]:
+                print(code[0], code[1])
                 return render_template("index.html")
     return render_template('log_in.html')
 
 
-@app.route("/admin"  , methods =["GET", "POST"])
+@app.route("/admin", methods=["GET", "POST"])
 def admin_page():
     return render_template("admin.html")
 
 
-@app.route("/Add_man")
+@app.route("/Add_man", methods=["GET", "POST"])
 def man_page():
+    file = request.files.get('avatar')
+    if file is not None:
+        new_filename = str(datetime.now().timestamp()).replace(
+            ".", "")  # Generating unique name for the file
+        # Spliting ORIGINAL filename to seperate extenstion
+        split_filename = file.filename.split(".")
+        # Canlculating last index of the list got by splitting the filname
+        ext_pos = len(split_filename)-1
+        # Using last index to get the file extension
+        ext = split_filename[ext_pos]
+        db_path = str(f"uploads/add_man/{new_filename}.{ext}")
+        print("The type of path  = ", type(db_path))
+        file.save(f"uploads/add_man/{new_filename}.{ext}")
+
+        print("File uploaded successfully")
+        print(request.form.get('name'), request.form.get("price"))
+        if request.method == 'POST':
+            print("post work")
+            with engine.connect() as conn:
+                query = text(
+                    "INSERT INTO add_man (item_name, price, descp , pic_name) VALUES (:item_name, :price, :descp, :pic_name)")
+                print("its between")
+                conn.execute(query, {"item_name": request.form.get('name'), "price": int(request.form.get(
+                    "price")), "descp": request.form.get("description"), "pic_name": db_path})
+                print("Data inserted successfully")
+    else:
+        print("Error: No file uploaded")
     return render_template("add_man.html")
 
-@app.route("/Add_woman")
+
+@app.route("/Add_woman", methods=["GET", "POST"] )
 def woman_page():
+    file = request.files.get('avatar')
+    if file is not None:
+        new_filename = str(datetime.now().timestamp()).replace(
+            ".", "")  # Generating unique name for the file
+        # Spliting ORIGINAL filename to seperate extenstion
+        split_filename = file.filename.split(".")
+        # Canlculating last index of the list got by splitting the filname
+        ext_pos = len(split_filename)-1
+        # Using last index to get the file extension
+        ext = split_filename[ext_pos]
+        db_path = str(f"uploads/add_woman/{new_filename}.{ext}")
+        print("The type of path  = ", type(db_path))
+        file.save(f"uploads/add_woman/{new_filename}.{ext}")
+
+        print("File uploaded successfully")
+        print(request.form.get('name'), request.form.get("price"))
+        if request.method == 'POST':
+            print("post work")
+            with engine.connect() as conn:
+                query = text(
+                    "INSERT INTO add_woman (item_name, price, descp , pic_name) VALUES (:item_name, :price, :descp, :pic_name)")
+                print("its between")
+                conn.execute(query, {"item_name": request.form.get('name'), "price": int(request.form.get(
+                    "price")), "descp": request.form.get("description"), "pic_name": db_path})
+                print("Data inserted successfully")
+    else:
+        print("Error: No file uploaded")
     return render_template("add_woman.html")
 
 
-
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
+    app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine, text
 import os
+import mysql.connector
 
-
-bd = "mysql+pymysql://bcqcnf0mqdgxjcuwtbqr:pscale_pw_VLiReyJTnDZgOJCYKVLzheKwf4KYxQdQupkvUk7EzJB@aws.connect.psdb.cloud/first-data-base?charset=utf8mb4"
+bd = "mysql+pymysql://lkwp097ctrdqyazqlzcq:pscale_pw_LtMfaJT1s4JfNPi5Q9ck6s2VAqYa8cRbNMsyY4C0x13@aws.connect.psdb.cloud/first-data-base?charset=utf8mb4"
 engine = create_engine(
   bd , connect_args={
     "ssl":{
@@ -13,15 +13,12 @@ engine = create_engine(
 
 def log_in_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from man_image"))
+    result = conn.execute(text("select * from sign_table"))
     log_in = []
     for row in result.all():
       log_in.append(row)
     return log_in
 
-# a = load_jobs_from_db()
-# print(a)
-  
   
 def add_sign_table_to_db(data):
     with engine.connect() as conn:
@@ -30,19 +27,3 @@ def add_sign_table_to_db(data):
     conn.execute(query, {"email_add": data['email'], "password_has": data["password"], "address1": data["address1"], "address2": data["address2"], "city": data["city"]})
 
 print("This working")
-
-
-
-  
-# def sign_store_in_db():
-#     with engine.connect() as conn:
-          
-  
-# def get_password():
-#     with engine.connect() as conn:
-#       result = conn.execute(text("select * from loge_in"))
-#       log_in = []
-#       for row in result.all():
-#         log_in.append(row)
-#     return log_in
-  
